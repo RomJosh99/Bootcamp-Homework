16b5888b30dc768ce1628ffdd94e17d8b76341f3
RomJosh99 <63078615+RomJosh99@users.noreply.github.com>
2020-06-14 21:57:02 -0500

    Pushing files

--- 04-web-apis/script.js
+++ 04-web-apis/script.js
@@ -1,23 +1,37 @@
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+var quizQuestions = {
+    questionOne:"Commonly used data types do NOT include.", 
+    oneAnswers: ["Strings","Booleans","Alerts","Numbers"],
+    answerOne:"Booleans",
 
-    submitButton.onclick = function(){
-		startQuiz(questions, quizContainer, resultsContainer);
-	}
+    questionTwo:"The condition in an if / else statement is enclosed within.", 
+    twoAnswers: ["Quotes","Curly brackets","Parenthesis","Square brackets"],
+    answerTwo:"2",
 
-	function showQuestions(questions, quizContainer){
-		var questions = [{
-            questions: "what is 1+1?",
-            answerChoice: ["2","3","4"],
-            answer: "2"
-        },
-        [{
-            questions: "what is 2+2?",
-            answerChoice:["2","3","4"],
-            answer: "4"}],
-        ]
-	}
+    questionThree:"The condition in an if / else statement is enclosed within.", 
+    twoAnswers: ["Quotes","Curly brackets","Parenthesis","Square brackets"],
+    answerTwo:"2",
 
-	
+    questionFour:"The condition in an if / else statement is enclosed within.", 
+    twoAnswers: ["Quotes","Curly brackets","Parenthesis","Square brackets"],
+    answerTwo:"2",
+
+    questionFive:"The condition in an if / else statement is enclosed within.", 
+    twoAnswers: ["Quotes","Curly brackets","Parenthesis","Square brackets"],
+    answerTwo:"2",
 }
 
 
+
+function startQuiz() {
+    document.getElementById("question").innerHTML = quizQuestions.questionOne;
+    document.getElementById("answer").innerHTML = quizQuestions.oneAnswers[0];
+    document.getElementById("answer2").innerHTML = quizQuestions.oneAnswers[1];
+    document.getElementById("answer3").innerHTML = quizQuestions.oneAnswers[2];
+    document.getElementById("answer4").innerHTML = quizQuestions.oneAnswers[3];
+    document.getElementById("answerButton2").innerHTML = quizQuestions.answerOne[3];
+
+
+}
+function answerButton1() {
+
+}
